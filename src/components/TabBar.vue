4734a429cb26deedeb121a56c77b550a875203b6
minzikimi <minji.kim@hyperisland.se>
2026-06-08 14:17:47 +0200

    add dark mode to tab bar comp

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -5,18 +5,18 @@
       currentPath !== '/signup' &&
       currentPath !== '/login'
     "
-    class="absolute bottom-0 w-full bg-white border-t border-[#E5E5EA] flex justify-around items-center h-20 z-40"
+    class="w-full bg-white dark:bg-neutral-800 border-t border-[#E5E5EA] dark:border-neutral-700 flex justify-around items-center h-20 z-40 transition-colors duration-300"
   >
     <router-link
       v-for="tab in tabs"
       :key="tab.path"
       :to="tab.path"
-      class="flex flex-col items-center justify-center text-xs text-gray-400 flex-1 h-full cursor-pointer transition-colors duration-150"
-      active-class="text-orange-500 font-semibold"
+      class="flex flex-col items-center justify-center text-sm text-gray-400 dark:text-neutral-500 flex-1 h-full cursor-pointer transition-colors duration-150"
+      active-class="text-orange-500 dark:text-orange-400 font-semibold"
       exact
     >
       <Icon :icon="tab.icon" width="26" height="26" />
-      <span class="mt-1.5 tracking-tight">{{ tab.label }}</span>
+      <span class="mt-1 tracking-tight">{{ tab.label }}</span>
     </router-link>
   </nav>
 </template>
